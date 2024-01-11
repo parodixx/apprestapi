9494774e547667774222f0afe29213a6c5e97710
parodixx <muhamadiqbalmufyandi@gmail.com>
2024-01-11 11:37:25 +0700

    menambahkan file koneksi

---
+++ koneksi.js
@@ -0,0 +1,17 @@
+var mysql = require('mysql');
+
+//buat koneksi database
+
+const conn = mysql.createConnection({
+    host:'localhost',
+    user:'root',
+    password:'',
+    database:'dbapi'
+});
+
+conn.connect((err)=>{
+    if(err) throw err;
+    console.log('Terhubung ke database')
+});
+
+module.exports = conn;
